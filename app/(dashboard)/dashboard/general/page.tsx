77c4b84cb42cbc8fbd0d69809d81d22f3a0adb4b
Zak DeBrine <zdebrine@gmail.com>
2025-05-08 13:13:47 -0500

    Removed console log

--- app/(dashboard)/dashboard/general/page.tsx
+++ app/(dashboard)/dashboard/general/page.tsx
@@ -36,8 +36,6 @@ export default function GeneralPage() {
     });
   };
 
-  console.log(user);
-
   return (
     <section className="flex-1 p-4 lg:p-8">
       <h1 className="text-lg lg:text-2xl font-medium text-gray-900 mb-6">
