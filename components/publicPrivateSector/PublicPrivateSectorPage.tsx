f06409772678fe93bb9c5d6532a158ff0044debf
walkingwisedev <walkingwisedev@gmail.com>
2025-10-06 15:07:00 -0600

    Update PublicPrivateSectorPage.tsx

--- components/publicPrivateSector/PublicPrivateSectorPage.tsx
+++ components/publicPrivateSector/PublicPrivateSectorPage.tsx
@@ -102,7 +102,7 @@ const PublicPrivateSectorPage = () => {
             Schedule
           </a>{" "}
           a platform tour today to discover how Walking Wise can support your
-          organization's education initiatives.<br><br>
+          organization's education initiatives.<br></br><br></br>
           <a
             href="https://www.walkingwise.com/pricing#ENTERPRISE"
             target="_self"
