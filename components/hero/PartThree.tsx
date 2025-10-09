6ce87f7961ff6815501b7e73db0f415ca8d8a32c
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 11:57:09 -0600

    Update PartThree.tsx

--- components/hero/PartThree.tsx
+++ components/hero/PartThree.tsx
@@ -35,62 +35,6 @@ const LogoItem = ({
 const PartThree = () => {
   return (
     <>
-    <div className="flex flex-col lg:flex-row max-w-screen-2xl mx-auto justify-evenly  px-4 sm:px-20 py-10 lg:py-32">
-      {/* Text Content */}
-      <div className="text-[#303030] flex flex-col justify-center items-center w-full lg:w-auto">
-        <h1 className="text-4xl sm:text-6xl px-4 max-w-4xl sm:px-8 text-left lg:text-center mb-2">
-          Postgraduate Institute for Medicine & Academy of Forensic Nursing
-        </h1>
-
-        {[
-          "Walking Wise has developed evidence-based education for adults and adolescents in collaboration with survivor leaders and advocacy experts, to help deliver content that is sensitive to the effects of trauma.",
-          "Our online course is accredited for Continuing Education (CME/CE) credits by the Postgraduate Institute for Medicine (PIM) and the Academy of Forensic Nursing (AFN).",
-        ].map((paragraph, index) => (
-          <p
-            key={index}
-            className={`${
-              robotoopo.className
-            } tracking-wide md:leading-7 lg:leading-12 text-left lg:text-center mb-2 text-xl sm:text-3xl px-4 sm:px-8 mt-4 sm:mt-${
-              index === 0 ? "15" : "5"
-            } text-center lg:text-left`}
-          >
-            {paragraph}
-          </p>
-        ))}
-      </div>
-
-      {/* Logo Images */}
-      <div className="flex flex-row lg:flex-col justify-center md:items-center gap-6 lg:gap-10 mt-10 lg:mt-0">
-        <div className="flex flex-col items-center w-1/2">
-          <LogoItem
-            src="/assets/home-1.png"
-            alt="Logo"
-            text="PIM Accreditation"
-            textColor="rgb(27, 68, 116)"
-            link="https://walking-wise-2025-website-assets.s3.us-east-1.amazonaws.com/Website_Dashboard-Education_PDFs_and_Icons/PIM_Accreditation_Info_Walking-Wise-Website/content/index.html#/"
-          />
-          <button
-            onClick={() =>
-              (window.location.href =
-                "/adult-education")
-            }
-            className={`bg-[#9d1be3] ${roboto.className} mt-4 rounded-full text-sm sm:text-[16px] py-2 sm:py-3 px-4 text-white flex items-center whitespace-nowrap`}
-          >
-            Online Course for Adults
-          </button>
-        </div>
-        <div className="flex flex-col items-center w-1/2">
-          <LogoItem
-            src="/assets/home-2.png"
-            alt="Logo"
-            text="AFN Partnership"
-            textColor="rgb(86, 1, 119)"
-            link="https://www.goafn.org/news-1/afn-and-walking-wise-help-schools-build-an-anti-sex-trafficking-culture"
-          />
-        </div>
-      </div>
-    </div>
-
     <div className="grid grid-cols-2 md:grid-cols-6     gap-5  items-start  px-4 sm:px-20 py-10 lg:py-32">
       {/* Text Content */}
       <div className="text-[#303030] 
@@ -154,7 +98,7 @@ const PartThree = () => {
               (window.location.href =
                 "/adult-education")
             }
-            className={`bg-[#9d1be3] ${roboto.className} mt-4 rounded-full text-sm sm:text-[16px] py-2 sm:py-3 px-4 text-white flex items-center whitespace-nowrap`}
+            className={`bg-[#9d1be3] ${roboto.className} block mx-auto mt-4 rounded-full text-sm sm:text-[16px] py-2 sm:py-3 px-4 text-white flex items-center whitespace-nowrap`}
           >
             Online Course for Adults
           </button>
