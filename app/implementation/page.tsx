fa56c071bab1c4ab3cd4a5045af012e40a285741
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 21:30:26 -0600

    Update page.tsx

--- app/implementation/page.tsx
+++ app/implementation/page.tsx
@@ -1,5 +1,11 @@
 import Hero from '@/components/implementation/Hero'
 import React from 'react'
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Implementation | Build a Sustainable Exploitation Education Program',
+  description: 'Follow five effective steps to launch a long-term child exploitation prevention program in your school or youth organization.',
+};
 
 const page = () => {
   return <Hero/>
