852a464f2908cd1dbcd72c38446420e56bda248e
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 18:12:05 -0600

    Update YouthOrganization.tsx

--- components/organization/YouthOrganization.tsx
+++ components/organization/YouthOrganization.tsx
@@ -201,11 +201,11 @@ const YouthOrganization = () => {
             Join our Affiliate Program to raise funds for your organization while advancing prevention education in your community. Receive your organization's unique link, share it with others, and help bring life-changing learning to schools and youth networks. 
           
            <div className="flex justify-center items-center  ">
-              <button 
+              <Button 
                 className={`text-wrap md:text-nowrap`}
                 onClick={() => window.open("https://tally.so/r/w459Bo", "_blank")}
                 >Apply to Join our Affiliate Program
-            </button>
+            </Button>
            </div>
           </BodyText>
             
