e5102414b1714330265ed01fbeee413d11784a3b
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 21:33:05 -0600

    Update page.tsx

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,5 +1,11 @@
 import Hero from '@/components/videos/Hero'
 import React from 'react'
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Animated Explainer Videos | Teach Youth About Online Safety & Exploitation',
+  description: 'Empower youth with engaging, 3-minute videos that teach how to spot unsafe adults, identify online predators, and seek help from trustworthy adults.',
+};
 
 const page = () => {
   return <Hero/>
