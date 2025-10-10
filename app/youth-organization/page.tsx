cf568b722235b8e0ed01b5ad68255c16d97f1fbc
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 21:29:10 -0600

    Update page.tsx

--- app/youth-organization/page.tsx
+++ app/youth-organization/page.tsx
@@ -1,5 +1,11 @@
 import Hero from '@/components/organization/Hero'
 import React from 'react'
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Youth Organization Curriculum | Sexual Exploitation Education',
+  description: 'Use adaptable lessons to educate youth in mentoring, small groups, or workshops on recognizing and preventing child exploitation.',
+};
 
 const page = () => {
   return <Hero/>
