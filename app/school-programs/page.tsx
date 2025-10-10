434d9c545d8c8e012fdfcf48a9a8a4dfb9cefe88
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 21:29:52 -0600

    Update page.tsx

--- app/school-programs/page.tsx
+++ app/school-programs/page.tsx
@@ -1,5 +1,11 @@
 import Hero from '@/components/school/Hero'
 import React from 'react'
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'School Programs | Empower Students to Recognize & Avoid Exploitation ',
+  description: 'Integrate exploitation-prevention education across grade levels with flexible, school-led lessons that build awareness and resilience.',
+};
 
 const page = () => {
   return <Hero/>
