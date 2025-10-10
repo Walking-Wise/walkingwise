4f01e1b7fb3c1b3ea95b5d596f14476f837dc82f
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 21:21:33 -0600

    Update page.tsx

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,6 +2,12 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import React from "react";
 import PricingPage from "@/components/pricing/PricingPage";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Walking Wise Pricing | Subscribe to Support Child Exploitation Prevention',
+  description: 'Access practical education tools while supporting our mission to protect youth from sexual predators and traffickers.',
+};
 
 const page = () => {
   return (
