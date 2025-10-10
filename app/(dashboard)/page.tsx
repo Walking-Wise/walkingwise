a7addf48c3780839dfb32d2e74b6e72e5f49c83f
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 21:46:28 -0600

    Update page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,10 @@
 import Hero from "@/components/hero/Hero";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Walking Wise® | Prevent Child Exploitation Through Education ',
+  description: 'Discover how Walking Wise equips adults and communities to teach youth (ages 11+) how to recognize, avoid, and report sexual predators and traffickers.',
+};
 
 export default function Main() {
   return (
