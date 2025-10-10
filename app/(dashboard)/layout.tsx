3829f79cc90e1e370233c3422812468c0e919575
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 20:50:33 -0600

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,28 +1,5 @@
 // app/layout.tsx (Server Component)
-import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://www.walkingwise.com"),
-  title: "Walking Wise",
-  description: "Build an Anti-trafficking culture with Walking Wise",
-  openGraph: {
-    type: "website",
-    url: "https://your-domain.com",
-    title: "Walking Wise",
-    description: "Build an Anti-trafficking culture with Walking Wise",
-    images: [
-      "https://walking-wise-2025-website-assets.s3.us-east-1.amazonaws.com/Website+Hero+Banners+300dpi/HOME_300dpi_1200x511_TEXT.jpg",
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Walking Wise",
-    description: "Build an Anti-trafficking culture with Walking Wise",
-    images: [
-      "https://walking-wise-2025-website-assets.s3.us-east-1.amazonaws.com/Website+Hero+Banners+300dpi/HOME_300dpi_1200x511_TEXT.jpg",
-    ],
-  },
-};
 
 export default function RootLayout({
   children,
