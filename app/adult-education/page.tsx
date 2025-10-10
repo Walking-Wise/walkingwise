637a0958e09a6b71d311e29ad0b6f6d6339b77cd
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 21:15:34 -0600

    Update page.tsx

--- app/adult-education/page.tsx
+++ app/adult-education/page.tsx
@@ -1,5 +1,11 @@
 import Hero from '@/components/adult/Hero'
 import React from 'react'
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Adult Education Courses | Continuing Education on Child Exploitation',
+  description: 'Earn continuing education credits with 12 learning modules designed for professionals and community members to recognize and respond to child sexual exploitation.',
+};
 
 const page = () => {
   return <Hero/>
