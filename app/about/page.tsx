9c0cdeb752d659ee7af2d044b38e873f85971519
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 20:59:32 -0600

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,10 @@
 import Hero from '@/components/about/Hero'
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'About Walking Wise® | Experts Unit to Help Prevent Child Exploitation',
+  description: 'Learn how 60+ experts partnered with Walking Wise to deliver evidence-based education, protecting children from sexual exploitation and trafficking.',
+};
 
 const about = () => {
   return (
