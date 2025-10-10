0bbf244fd76e7a2ef98e525d4a8647af2c5b6deb
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 21:26:01 -0600

    Update page.tsx

--- app/public-private-sector/page.tsx
+++ app/public-private-sector/page.tsx
@@ -1,5 +1,11 @@
 import Hero from '@/components/publicPrivateSector/Hero'
 import React from 'react'
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Corporate & Organizational Training | Recognize Sexual Exploitation',
+  description: 'Empower your employees with evidence-based education to help them detect and respond to sexual abuse and child exploitation.',
+};
 
 const page = () => {
   return <Hero/>
