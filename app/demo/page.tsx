63b87b6a877d6cee8d89ee1f4b529ae452b2d474
walkingwisedev <walkingwisedev@gmail.com>
2025-10-09 21:16:32 -0600

    Update page.tsx

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,5 +1,11 @@
 import Hero from '@/components/demo/Hero'
 import React from 'react'
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Course Demo | Experience Walking Wise’s Myths & Reality Lesson',
+  description: 'Take the hands-on demo of our adult learning course, Myths & Reality, and see how prevention education transforms understanding.',
+};
 
 const page = () => {
   return <Hero/>
