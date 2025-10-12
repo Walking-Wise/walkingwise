3f0c6def47b10d68bc40526b46b0d237bed9bdd3
walkingwisedev <walkingwisedev@gmail.com>
2025-10-12 11:08:39 -0600

    Update page.tsx

--- app/youth-curriculum/page.tsx
+++ app/youth-curriculum/page.tsx
@@ -1,6 +1,11 @@
 import Hero from '@/components/youth/Hero'
 import React from 'react'
-
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Youth Curriculum | Exploitation Education for Ages 11+',
+  description: 'Engage middle & high school youth with interactive lessons & animated videos that build awareness and confidence in recognizing sexual exploitation.',
+};
 const page = () => {
   return <Hero/>
 }
