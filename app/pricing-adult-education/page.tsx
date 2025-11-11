13929f13bcaf2024c990e6f4a5538bebaaeefcff
walkingwisedev <walkingwisedev@gmail.com>
2025-11-11 12:28:17 -0700

    Update page.tsx

--- app/pricing-adult-education/page.tsx
+++ app/pricing-adult-education/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import React from "react";
-import PricingPage from "@/components/pricing-adult-education/PricingAdult";
+import PricingAdult from "@/components/pricing-adult-education/PricingAdult";
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
