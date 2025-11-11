482f7db842853ed6b6a25e2dd48a40d04824c3e8
walkingwisedev <walkingwisedev@gmail.com>
2025-11-11 11:57:58 -0700

    Update page.tsx

--- app/pricing-adult-education/page.tsx
+++ app/pricing-adult-education/page.tsx
@@ -1,1 +1,22 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import React from "react";
+import PricingPage from "@/components/pricing-adult-education/PricingAdult";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Walking Wise Pricing for Adult Education',
+  description: 'Access practical education tools while supporting our mission to protect youth from sexual predators and traffickers.',
+};
 
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <PricingPage />
+      <Footer />
+    </>
+  );
+};
+
+export default page;
