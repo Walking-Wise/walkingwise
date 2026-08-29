e7799c9a99bf273f8e0ce9809b7d73849bd3222f
walkingwisedev <walkingwisedev@gmail.com>
2026-08-29 21:03:41 +0300

    Update SchoolDistrictPrograms.tsx

--- components/school/SchoolDistrictPrograms.tsx
+++ components/school/SchoolDistrictPrograms.tsx
@@ -17,7 +17,7 @@ import Section from "../ui/Section/Section";
   const blueboxstyle = {
     paddingLeft: '3vw !important',
   };
-  const bluetextitalic = { color: '#68d2df'};
+  const bluetext = { color: '#68d2df'};
 
 const cardDataTop = [
   {
@@ -300,9 +300,9 @@ const SchoolDistrictPrograms = () => {
           if  (feature.includes("TRACK"))
            return <li><h1 id={card.title} className="text-4xl sm:text-6xl">{feature}</h1></li>;
           else if  (feature.includes("th GRADE"))
-           return <li className="text-xl sm:text-2xl md:text-2xl text-bold text-italic" style="{bluetextitalic}" >{feature}</li>;
+           return <li className="text-xl sm:text-2xl md:text-2xl text-bold text-italic" style={bluetext} >{feature}</li>;
           else if  (feature.includes("PARENTS"))
-           return <li className="text-xl sm:text-2xl md:text-2xl text-bold text-italic" style="{bluetextitalic}" >{feature}</li>;
+           return <li className="text-xl sm:text-2xl md:text-2xl text-bold text-italic" style={bluetext} >{feature}</li>;
           else if  (feature.includes("Recommended Implementations"))
            return <h2 className="text-xl sm:text-2xl md:text-2xl text-bold text-italic" >{feature}</h2>;
           
