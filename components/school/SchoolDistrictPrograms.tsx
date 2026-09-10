325aaddc3335f5eda6f0858fb6004d223f01d6c8
walkingwisedev <walkingwisedev@gmail.com>
2026-09-10 17:46:25 +0300

    Update SchoolDistrictPrograms.tsx

--- components/school/SchoolDistrictPrograms.tsx
+++ components/school/SchoolDistrictPrograms.tsx
@@ -309,12 +309,12 @@ const SchoolDistrictPrograms = () => {
            return <h1 className="text-3xl sm:text-4xl md:text-6xl text-[#303030]">Learning Journey</h1>;
           
           else if (feature === "BUTTON1")
-          return <li className="line-short" ><button type="button" className="bg-[#9d1be3] mx-auto cursor-pointer  text-white  px-6 md:px-10 py-2 md:py-4  rounded-4xl  transition-colors  md:text-lg  whitespace-nowrap  flex items-center justify-center roboto_8d2838e4-module__oXDxdq__className">ENROLL Today!&nbsp;&nbsp;</button>
+          return <li className="line-short" ><a href="https://www.walkingwise.com/pricing-youth-curriculum" target="_blank" className="bg-[#9d1be3] mx-auto cursor-pointer  text-white  px-6 md:px-10 py-2 md:py-4  rounded-4xl  transition-colors  md:text-lg  whitespace-nowrap  flex items-center justify-center roboto_8d2838e4-module__oXDxdq__className">ENROLL Today!&nbsp;&nbsp;</a>
          </li>;
 
           else if (feature === "BUTTON2")
           return <li className="line-short" >
-         <button type="button" className="bg-[#9d1be3] mx-auto cursor-pointer  text-white  px-6 md:px-10 py-2 md:py-4  rounded-4xl  transition-colors  md:text-lg  whitespace-nowrap  flex items-center justify-center roboto_8d2838e4-module__oXDxdq__className">Teaching Track</button>
+         <a href="https://walking-wise-assets.s3.us-east-1.amazonaws.com/Learning+Journey+Walking+Wise.pdf" target="_blank" className="bg-[#9d1be3] mx-auto cursor-pointer  text-white  px-6 md:px-10 py-2 md:py-4  rounded-4xl  transition-colors  md:text-lg  whitespace-nowrap  flex items-center justify-center roboto_8d2838e4-module__oXDxdq__className">Teaching Track</a>
          </li>;
 
           else if (feature === " ")
